d660a2ffa1a86aa7c8e4fa6e8ee714aa8a214794
Giulia Gogna <giulia.gogna@gmail.com>
2026-06-09 13:54:17 +0200

    Clean comments in std_impl.cpp and rename it to stb_implementation.cpp [skipci]

--- src/stb_implementation.cpp
+++ src/stb_implementation.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2026 Giulia Gogna, Riccardo Piazza.
+ * Copyright (c) 2026 Giulia Gogna, Riccardo Piazza.
  *
  * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
  * the European Commission - subsequent versions of the EUPL (the "Licence");
@@ -15,11 +15,8 @@
  * limitations under the Licence.
  */
 
-// This file is the only that includes the implementation of the stb libraries,
-// so that we don't have to include them in every file that uses them.
-// The stb libraries are header-only, so we need to define the implementation in one file.
-
-// GG: is it really necessary given that we will only use this in HDRImage?
+// This translation unit instantiates the implementation of the stb_image_write library.
+// The implementation macro must appear exactly once in the entire program.
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb/stb_image_write.h>
